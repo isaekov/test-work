f41e9cbc791e7d2ca0dfaa4f7c7455a66f2a2c65
ildar <derorn@mail.ru>
2020-08-02 11:25:55 +0300

    finish

--- src/public/index.php
+++ src/public/index.php
@@ -1,54 +1,44 @@
 <?php
 
-use TestWork\Calculator;
-
+use TestWork\{Calculator, Cart};
 use TestWork\discount\DiscountManager;
-use TestWork\Cart;
 use TestWork\product\Product;
-use TestWork\rule\DiscountRule;
-use TestWork\rule\SpecialDiscountRule;
-use TestWork\rule\CountDiscountRule;
+use TestWork\rule\{DiscountRule, SpecialDiscountRule, CountDiscountRule};
 
 require_once __DIR__ . "/../vendor/autoload.php";
 
-
-$productA = new Product('A', 100);
-$productB = new Product('B', 100);
-$productC = new Product('C', 100);
-$productD = new Product('D', 100);
-$productE = new Product('E', 100);
-$productF = new Product('F', 100);
-$productG = new Product('G', 100);
-$productH = new Product('H', 100);
-$productI = new Product('I', 100);
-$productJ = new Product('J', 100);
-$productK = new Product('K', 100);
-$productL = new Product('L', 100);
-$productM = new Product('M', 100);
-
+$A = new Product('A', 10);
+$B = new Product('B', 10);
+$C = new Product('C', 10);
+$D = new Product('D', 10);
+$E = new Product('E', 10);
+$F = new Product('F', 10);
+$G = new Product('G', 10);
+$H = new Product('H', 10);
+$I = new Product('I', 10);
+$J = new Product('J', 10);
+$K = new Product('K', 10);
+$L = new Product('L', 10);
+$M = new Product('M', 10);
 
 
 $discountManager = new DiscountManager();
-$discountManager->add(new DiscountRule([$productA, $productB], 0.1));
-$discountManager->add(new DiscountRule([$productD, $productE], 0.05));
-$discountManager->add(new DiscountRule([$productE, $productF, $productM], 0.05));
-$discountManager->add(new SpecialDiscountRule($productA, [$productK, $productL, $productM], 0.05));
-$discountManager->add(new CountDiscountRule(3, [$productA, $productC], 0.05));
-$discountManager->add(new CountDiscountRule(4, [$productA, $productC], 0.1));
-$discountManager->add(new CountDiscountRule(5, [$productA, $productC], 0.2));
+// The rule
+$discountManager->add(new DiscountRule([$A, $B], 0.1));
+$discountManager->add(new DiscountRule([$D, $E], 0.06));
+$discountManager->add(new DiscountRule([$E, $F, $G], 0.03));
+$discountManager->add(new SpecialDiscountRule($A, [$K, $L, $M], 0.05));
+$discountManager->add(new CountDiscountRule(3, [$A, $C], 0.05));
+$discountManager->add(new CountDiscountRule(4, [$A, $C], 0.1));
+$discountManager->add(new CountDiscountRule(5, [$A, $C], 0.2));
 
 $calculator = new Calculator();
 $calculator->setDiscountManager($discountManager);
 
 
 $cart = new Cart();
-$cart->addProduct($productA);
-$cart->addProduct($productD);
-$cart->addProduct($productE);
-$cart->addProduct($productB);
-$cart->addProduct($productA);
-$cart->addProduct($productD);
-$cart->addProduct($productD);
+$cart->addProduct($A);
+$cart->addProduct($B);
 
 $calculator->setOrder($cart);
 $res = $calculator->calculate();
